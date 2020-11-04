4a115adedb3838b4b31862dbb0c33ef448f39fbe
dikshitakambri <dikshitakambri18@gmail.com>
2020-11-04 16:48:47 +0530

    first story

--- Story.js
+++ Story.js
@@ -5,10 +5,23 @@ let lightning = document.querySelector("#lightning");
 
 //Setup animation code here
 
-var lightningStart = {"left":"290px"};
-var lightningEnd = {"left":"900px"};
-var option = {"duration": 1000}
+let lightningStart = {"left":"290px"};
+let lightningEnd = {"left":"900px"};
+let options = {"duration": 1000};
+
+let hitTheVillain = () => {
+    let villainStart = {
+        transform : 'rotate(0deg)',
+        opacity : 100
+    };
+    let villainEnd = {
+        transform : 'rotate(1000deg)',
+        opacity : 0
+    };
+    let options = {"duration":1500 };
+    villain.animate([villainStart,villainEnd],options);
+}
 
 //Run animation code here
 
-lightning.animate([lightningStart,lightningEnd],option)
+lightning.animate([lightningStart,lightningEnd],options).onfinish = hitTheVillain;
